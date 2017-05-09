eda155cb3e9a090431edd7a35d42b8ccd5b14ac3
icebangbang <787036156@qq.com>
2017-05-09 12:16:38 +0800

    model request param bug fix

--- maas-web/src/main/java/com/ecreditpal/maas/web/endpoint/ApiServiceEndpoint.java
+++ maas-web/src/main/java/com/ecreditpal/maas/web/endpoint/ApiServiceEndpoint.java
@@ -4,6 +4,7 @@ package com.ecreditpal.maas.web.endpoint;
 import com.ecreditpal.maas.common.IPBasedRateLimiter;
 import com.ecreditpal.maas.common.avro.LookupEventMessage.LookupEventMessage;
 import com.ecreditpal.maas.common.utils.http.OkHttpUtil;
+import com.ecreditpal.maas.common.utils.json.JsonUtil;
 import com.ecreditpal.maas.model.bean.Result;
 import com.ecreditpal.maas.model.bean.XYBModelBean;
 import com.ecreditpal.maas.service.ModelService;
@@ -36,13 +37,16 @@ public class ApiServiceEndpoint {
 
     @POST
     @Path("/{apiCode}")
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Produces(MediaType.APPLICATION_JSON)
     @ApiOperation(value = "Returns model result by apiCode", notes = "Returns a model result by json", response = Result.class)
     public Result<Object> getModelResult(
             @ApiParam(name = "apiCode", value = "ecreditpal api code", required = true) @PathParam("apiCode") String apiCode,
             @Context LookupEventMessage lookupEventMessage) {
-        //得到请求参数
-        Map<String, String> map = FilterUtil.getRequestForm(request,providers);
+//        String ss = request.getParameter("creditLimit");
+//        //得到请求参数
+//        Map<String, String> map = FilterUtil.getRequestForm(request,providers);
+        Map<String,String> map = JsonUtil.json2Map(lookupEventMessage.getRequestInfo().getFormParams().toString());
         //获得apiCode对应的模型
          ModelService service = ServiceContainer.getModelService(apiCode);
 
