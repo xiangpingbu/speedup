a4892978e882768f27cb2dcfc6d8132a2c63483b
icebangbang <787036156@qq.com>
2017-05-13 16:17:14 +0800

    model execute structure refresh

--- maas-service/src/main/java/com/ecreditpal/maas/service/ScoreCardService.java
+++ maas-service/src/main/java/com/ecreditpal/maas/service/ScoreCardService.java
@@ -2,22 +2,38 @@ package com.ecreditpal.maas.service;
 
 import com.ecreditpal.maas.common.avro.LookupEventMessage.LookupEventMessage;
 import com.ecreditpal.maas.common.avro.LookupEventMessage.ModelLog;
+import com.ecreditpal.maas.common.utils.ClassUtil;
 import com.ecreditpal.maas.model.model.AKDModel;
 import com.ecreditpal.maas.model.model.XYBModel;
 import com.ecreditpal.maas.service.annotation.Model;
+import com.ecreditpal.maas.service.annotation.ModelApi;
 import org.apache.avro.generic.GenericRecord;
 
+import javax.xml.bind.JAXBException;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @author lifeng
  * @CreateTime 2017/4/13.
  */
-@Model(apiCode = "M112")
-public class AkdService implements ModelService {
+@Model
+public class ScoreCardService  {
 
-    @Override
-    public Object getResult(Map<String, String> map, GenericRecord record) {
+    @ModelApi(apiCode = "M111")
+    public Object xybService(Map<String, String> map, GenericRecord record) {
+        XYBModel xybModel = new XYBModel();
+        String score = xybModel.run(map).toString();
+
+        LookupEventMessage lookupEventMessage = (LookupEventMessage)record;
+        ModelLog modelLog = xybModel.ParseVariables(xybModel.getVariableList(), score, XYBModel.XYBModelVariables.getModel());
+        lookupEventMessage.setModelLog(modelLog);
+
+        return score;
+    }
+
+    @ModelApi(apiCode = "M112")
+    public Object akdService(Map<String, String> map, GenericRecord record) {
         AKDModel akdModel = new AKDModel();
         String score = akdModel.run(map).toString();
 
--- maas-service/src/main/java/com/ecreditpal/maas/service/ServiceContainer.java
+++ maas-service/src/main/java/com/ecreditpal/maas/service/ServiceContainer.java
@@ -2,9 +2,13 @@ package com.ecreditpal.maas.service;
 
 import com.ecreditpal.maas.common.utils.ClassUtil;
 import com.ecreditpal.maas.service.annotation.Model;
+import com.ecreditpal.maas.service.annotation.ModelApi;
 import com.google.common.collect.Maps;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.generic.GenericRecord;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.List;
 import java.util.Map;
 
@@ -14,33 +18,47 @@ import java.util.Map;
  */
 @Slf4j
 public class ServiceContainer {
-    private static Map<String, ModelService> map = Maps.newHashMap();
+    private static Map<String, MethodClassMapping> containerMap = Maps.newHashMap();
 
     static {
+        try {
         /*找到com.ecreditpal.maas.service下所有的类*/
-        List<Class<?>> list = ClassUtil.getClasses("com.ecreditpal.maas.service");
-        for (Class<?> clz : list) {
-            Model model = clz.getAnnotation(Model.class);
-            if (model != null) {
-                try {
-                    //如果
-                    map.put(model.apiCode(), (ModelService) clz.newInstance());
-                    log.info("init modelService,apiCode:{}",model.apiCode());
-                } catch (Exception e) {
-                    throw new RuntimeException(e);
+            List<Class<?>> list = ClassUtil.getClasses("com.ecreditpal.maas.service");
+            for (Class<?> clz : list) {
+//            Model model = clz.getAnnotation(Model.class);
+//            Model model = clz.getAnnotation(Model.class);
+                Model model = clz.getAnnotation(Model.class);
+                Object obj = clz.newInstance();
+
+                if (model != null) {
+                    Method m[] = clz.getDeclaredMethods();
+                    for (Method method : m) {
+                        ModelApi modelApi = method.getAnnotation(ModelApi.class);
+                        //如果
+                        if (modelApi != null) {
+                            containerMap.put(modelApi.apiCode(), new MethodClassMapping(obj, method));
+                            log.info("init modelService,apiCode:{}", modelApi.apiCode());
+                        }
+                    }
                 }
+
             }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
         }
-        log.info("init {} modelService",map.size());
+        log.info("init {} modelService", containerMap.size());
     }
 
-    /**
-     * 根据apiCode获得模型的Service
-     * @param apiCode ecreditpal apicode
-     * @return ModelService
-     */
-    public static ModelService getModelService(String apiCode) {
-        return map.get(apiCode);
+    public static Object execute(String apiCode, Map<String,String> map,GenericRecord genericRecord) {
+        MethodClassMapping methodClassMapping =  containerMap.get(apiCode);
+        Method method = methodClassMapping.getMethod();
+        Object object = methodClassMapping.getObject();
+        try {
+           return method.invoke(object,map,genericRecord);
+        } catch (IllegalAccessException | InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
     }
+
 }
 
