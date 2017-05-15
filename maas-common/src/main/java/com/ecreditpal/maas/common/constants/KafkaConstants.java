c78b1485028bc94730b17e5426d978deceefa2c9
icebangbang <787036156@qq.com>
2017-05-15 16:45:48 +0800

    修改event tracking topic

--- maas-common/src/main/java/com/ecreditpal/maas/common/constants/KafkaConstants.java
+++ maas-common/src/main/java/com/ecreditpal/maas/common/constants/KafkaConstants.java
@@ -5,6 +5,6 @@ package com.ecreditpal.maas.common.constants;
  * @version 2017/4/12.
  */
 public class KafkaConstants {
-        public static final String KAFKA_LOOKUP_EVENT_TOPIC = "kafka_lookup_event";
+        public static final String KAFKA_LOOKUP_EVENT_TOPIC = "maas_kafka_lookup_event";
 
 }
