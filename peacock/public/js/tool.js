96fca23dd98b0162856f23a98ec5540f1de8903a
icebangbang <787036156@qq.com>
2017-05-18 14:55:39 +0800

    user counld get first td or second td

--- peacock/public/js/tool.js
+++ peacock/public/js/tool.js
@@ -825,7 +825,7 @@ define(['jquery', 'd3', 'tool_button'], function ($, d3, tool_button) {
                     key = getVarName(obj, headMap)
                 }
 
-                if (index == 1 || index == obj.length - 2) {
+                if (index == 0 || index == 1 || index == obj.length - 2) {
                     if (key == 'min_boundary') {
                         td = td.attr("name", key);
                     }
