0bc7c26e105da8f51c89966d9418b8288d30db21
jiaming.chen <jiaming.chen@ecreditpal.com>
2017-07-19 14:42:18 +0800

    modify sidebar styles and update package.json

--- peacock-v2/src/components/SideBar.vue
+++ peacock-v2/src/components/SideBar.vue
@@ -1,10 +1,20 @@
 <template>
-    <div class="sidebar">
+    <div class="sidebar" :class="{'toggle-hide': toggleStatus}">
         <!-- TODO use router config dynamic generate -->
+        <div class="sidebar-title">
+            <span class="title-name">icon | {{$route.params.id}}</span>
+            <a class="toggle-btn" @click="toggleSidebar">
+                <!-- TODO use icon  -->
+                &lt;
+            </a>
+        </div>
         <div v-for="item in routes" :key="item.title">
-            <div class="title">{{item.name}}</div>
+            <div class="block-title">{{item.name}}</div>
             <div class="item">
-                <router-link v-for="route in item.path" :key="route.name" :to="route.path">{{route.name}}</router-link>
+                <router-link v-for="route in item.path" :key="route.name" :to="route.path">
+                    icon |
+                    <span class="item-name">{{route.name}}</span>
+                </router-link>
             </div>
         </div>
     </div>
@@ -15,6 +25,7 @@ export default {
     name: 'sidebar',
     data() {
         return {
+            toggleStatus: false,
             routes: [
                 {
                     name: 'DATA',
@@ -62,22 +73,88 @@ export default {
             ]
         }
     },
+    methods: {
+        toggleSidebar() {
+            this.toggleStatus = !this.toggleStatus
+            this.$root.$emit('toggleStatus', this.toggleStatus)
+        }
+    },
     created() {
     }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 .sidebar {
     position: absolute;
     width: 280px;
     background: #fafafa;
     height: calc(100vh - 51px);
-}
-.sidebar .item a {
-    display: block;
-}
-.sidebar .item a.active {
-    background: #ffffff;
+    box-shadow: inset -1px 0 0 0 rgba(0, 0, 0, .075);
+    * {
+        white-space: nowrap;
+    }
+    &.toggle-hide {
+        width: 60px;
+        .sidebar-title {
+            width: 60px;
+        }
+        .toggle-btn {
+            transform: rotate(180deg);
+        }
+        .block-title, .title-name, .item-name {
+            display: none;
+        }
+    }
+
+    a:hover {
+        text-decoration: none;
+    }
+
+    .sidebar-title {
+        line-height: 54px;
+        padding-left: 15px;
+        font-weight: bold;
+        overflow: hidden;
+        border-bottom: 1px solid rgba(0, 0, 0, .075);
+        .toggle-btn {
+            width: 36px;
+            height: 34px;
+            line-height: 30px;
+            text-align: center;
+            float: right;
+            margin: 10px 14px;
+            border: 1px solid rgba(0, 0, 0, 0);
+            &:hover {
+                border-radius: 2px;
+                border: 1px solid rgba(0, 0, 0, .075);
+            }
+        }
+    }
+
+    .block-title {
+        line-height: 36px;
+        font-size: 12px;
+        font-weight: bold;
+        padding-left: 15px;
+    }
+    .item {
+        a {
+            color: #337ab7;
+            padding-left: 20px;
+            font-size: 15px;
+            line-height: 40px;
+            display: block;
+            &:hover {
+                background: #eee;
+            }
+        }
+        .router-link-active {
+            position: relative;
+            background: #ffffff;
+            padding-left: 15px;
+            border-left: 5px solid #337ab7;
+        }
+    }
 }
 </style>
--- peacock-v2/src/pages/project/Inner.vue
+++ peacock-v2/src/pages/project/Inner.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <Sidebar></Sidebar>
-    <div class="main">
+    <div class="main" :class="{'toggle-hide': toggleStatus}">
       <router-view></router-view>
     </div>
   </div>
@@ -11,19 +11,30 @@
 import Sidebar from '@/components/Sidebar.vue'
 export default {
   name: 'projectinner',
+  data() {
+    return {
+      toggleStatus: false
+    }
+  },
   created() {
     // default router, TODO: use router config
-    this.$router.push(`${this.$route.path}/datapreview`)
+    this.$router.push(`${this.$route.path}/datasource`)
+    this.$root.$on('toggleStatus', toggleStatus => {
+      this.toggleStatus = toggleStatus
+    })
   },
   components: {
     Sidebar
   }
 }
 </script>
-<style scoped>
+<style lang="scss" scoped>
 .main {
   margin-left: 280px;
   padding: 20px;
+  &.toggle-hide {
+    margin-left: 60px;
+  }
 }
 </style>
 
